470f73639ea6932270bedc30bb816ad56f110734
5onthediesel <weinshenker.brian@gmail.com>
2026-02-06 14:43:00 -0500

    ..

--- db.java
+++ db.java
@@ -3,42 +3,33 @@ import java.io.*;
 
 class db {
 
-    public static void main(String args[]) throws IOException
-    {
+    public static void main(String args[]) throws IOException {
         String url;
         Connection conn;
         PreparedStatement pStatement;
+        int result;
         ResultSet rs;
         String queryString;
 
-        try {
-            Class.forName("org.postgresql.Driver");
-        }
-        catch (ClassNotFoundException e) {
-            System.out.println("Failed to find the JDBC driver");
-        }
+        try { Class.forName("org.postgresql.Driver"); }
+        catch (ClassNotFoundException e) { System.out.println("Failed to find the JDBC driver"); }
 
-        try
-        {
+        try {
             url = "jdbc:postgresql://localhost:5432/postgres";
             conn = DriverManager.getConnection(url, "postgres", "rubiks");
 
             queryString = "drop table if exists cs370";
             pStatement = conn.prepareStatement(queryString);
-            int result = pStatement.executeUpdate();
-
-            queryString = "create table cs370 (latitude char(40), longitude char(40), url char(40))";
-            pStatement = conn.prepareStatement(queryString);
             result = pStatement.executeUpdate();
 
-
+            queryString = "create table cs370 (latitude char(40), longitude char(40), hash char(40), date char(40), time char(40))";
+            pStatement = conn.prepareStatement(queryString);
+            result = pStatement.executeUpdate();
         }
-        catch (SQLException se)
-        {
-            System.err.println("SQL Exception." +
-                    "<Message>: " + se.getMessage());
-        }
-
+        catch (SQLException se) { System.err.println("SQL Exception." + "<Message>: " + se.getMessage()); }
     }
 
+    public static Metadata get_metadata(File f) {
+
+    }
 }
